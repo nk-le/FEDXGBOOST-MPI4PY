8cf703c2d85869471a92a06ba9acbea453fe18f3
KhanhLn <khanhln2907@gmail.com>
2023-01-20 21:36:01 +0100

    added logs

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ modelArr = ["PlainXGBoost", "FedXGBoost", "SecureBoost", "PseudoSecureBoost", "F
 dataset = ["Iris", "GiveMeCredits", "Adult", "DefaultCredits", "AugData"]
 
 CONFIG = {
-  "model": modelArr[0],
+  "model": modelArr[1],
   "dataset": dataset[1],
   "lambda": 1,
-  "gamma": 0.4, # 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 5, 10  
-  "MAX_DEPTH": 8,
-  "MAX_TREE": 10
+  "gamma": 0.2, # 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 5, 10  
+  "MAX_DEPTH": 3,
+  "MAX_TREE": 6
 }
 
 class SIM_PARAM:
